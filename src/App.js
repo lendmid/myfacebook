c834b486cc2d269c436201576579da095477cc21
dmitriy <lendmid@bk.ru>
2020-07-27 23:36:32 +0300

    refactor

--- src/App.js
+++ src/App.js
@@ -3,18 +3,15 @@ import Header from './components/Header/Header';
 import Profile from './components/Profile/Profile';
 import Messages from './components/Messages/Messages';
 import './App.css';
-import {Route, BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 const App = (props) => {
-    // debugger
-    // let state = props.store.getState();
     return (
         <BrowserRouter>
             <div className="app-wrapper">
-                <Header/>
-                {/*<Route path='/messages' render={() => <Messages messagesPage={state.messagesPage} dispatch={state.dispatch}/>}/>*/}
-                <Route path='/messages' render={() => <Messages store={props.store}/>}/>
-                <Route path='/profile' render={() => <Profile store={props.store}/>}/>
+                <Header />
+                <Route path='/messages' render={() => <Messages store={props.store} />} />
+                <Route path='/profile' render={() => <Profile store={props.store} />} />
             </div>
         </BrowserRouter>
     )
--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import store from './redux/redux-store';
 
-let rerenderEntireTree = (state) => {
-    ReactDOM.render(<React.StrictMode><App store={store}/></React.StrictMode>,
+let rerenderEntireTree = () => {
+    ReactDOM.render(
+        <React.StrictMode><App store={store} /></React.StrictMode>,
         document.getElementById('root'));
 }
-// serviceWorker.unregister();
+
 
 rerenderEntireTree(store.getState());
 
