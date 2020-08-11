5d28147e74ded0185790b355a81442c9cb94de07
dmitriy <lendmid@bk.ru>
2020-08-12 00:29:58 +0300

    tried added axios, unsuccessfully

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import s from './Users.module.css';
+import {AxiosInstance as axios} from "axios";
 
 
 const Users = (props) => {
+    // debugger
+    
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        // debugger
+        console.log(response)
+    })
+    
     return (
         <div className={s.users}>
             {props.users}
