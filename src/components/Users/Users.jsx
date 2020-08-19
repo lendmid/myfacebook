54934e47c9696a33637a992347cdb466857f2742
dmitriy <lendmid@bk.ru>
2020-08-20 00:05:58 +0300

    added in Users firs life cycle methods - componentDidMount; refactoring

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,24 +4,20 @@ import axios from "axios";
 
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
         .then(response => {
             this.props.setUsers(response.data.items)
         })
     }
-
+    
     render() {
         return (
             <div className={s.users}>
                 {this.props.users}
-                {/*<button className={s.button} onClick={this.props.users}>Get users</button>*/}
             </div>
         )
     }
-    
 }
 
 export default Users;
