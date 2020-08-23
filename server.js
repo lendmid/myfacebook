b174968c1dbc9cf44fd7c32367f6040271b4f3b1
dmitriy <lendmid@bk.ru>
2020-08-23 23:45:09 +0300

    created first server

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 let express = require('express');
-
 let app = express();
-
-app.get('/', function (req, res) {
-    res.send('Hello API')
-})
 
 app.listen(3012, function () {
     console.log('API app started')
+})
+
+app.get('/', function (req, res) {
+    res.send('Hello API');
+})
+app.get('/users', function (req, res) {
+    res.send(users);
+})
+app.get('/users/:id', function (req, res) {
+    let user = users.find(function (user) {
+        return user.id === Number(req.params.id)
+    });
+    res.send(user);
+    console.log(user);
 })
 
 let users = [
