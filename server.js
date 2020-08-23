de8161618703baf3c1c036db2a0990e7e3aba122
dmitriy <lendmid@bk.ru>
2020-08-24 00:53:03 +0300

    implemented basic operations (CRUD)

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 let express = require('express');
+let bodyParser = require('body-parser');
+
 let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3012, function () {
     console.log('API app started')
@@ -17,6 +21,31 @@ app.get('/users/:id', function (req, res) {
     });
     res.send(user);
     console.log(user);
+})
+
+app.post('/users', function (req, res) {
+    let user = {
+        id: Date.now(),
+        name: req.body.name,
+    };
+    users.push(user);
+    res.send(user);
+})
+
+app.put('/users/:id', function (req, res) {
+    let user = users.find(function (user) {
+        return user.id === Number(req.params.id)
+    });
+    user.name = req.body.name;
+    res.send(user);
+    // res.sendStatus(200)
+})
+
+app.delete('/users/:id', function (req, res) {
+    users = users.filter(function (user) {
+        return user.id !== Number(req.params.id)
+    })
+    res.sendStatus(200)
 })
 
 let users = [
