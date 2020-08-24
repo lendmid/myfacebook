8c1e9503e98466d4d28f9bded83f86ea08c21255
dmitriy <lendmid@bk.ru>
2020-08-24 13:58:25 +0300

    tried added mongo db

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 let express = require('express');
 let bodyParser = require('body-parser');
+let MongoClient = require('mongodb').MongoClient; // подключение базы данных
 
 let app = express();
+let db;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.listen(3012, function () {
-    console.log('API app started')
-})
+// MongoClient.connect('mongodb://localhost:27017', (err, client) => {
+//     db = client.db('mytestingdb');
+//     app.listen(3012, function () {
+//         console.log('API app started')
+//     })
+// })
 
+MongoClient.connect('mongodb://localhost:27017', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}, (err, client) => {
+    if (err) return console.log(err);
+    db = client.db('first_bd')
+    app.listen(3012, function () {
+        console.log('API app started')
+    })
+})
+// .then((client) => {
+//     console.log('DB Connected!')
+//     app.listen(3012, function () {
+//         // console.log('API app started')
+//     })
+// }).catch(err => {
+//     console.log(`${err.message}`);
+// });
+
+// MongoClient.connect('mongodb://localhost:27017/myapi', function (err, database) {
+//     if (err) return console.log(err);
+//     db = database;
+//     app.listen(3012, function () {
+//         console.log('API app started')
+//     })
+// })
+///////// подкоючал базу данных
 app.get('/', function (req, res) {
     res.send('Hello API');
 })
