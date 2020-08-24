b01b55f42fa73d192ec35bd8a8e3604e24c04f1d
dmitriy <lendmid@bk.ru>
2020-08-24 20:17:00 +0300

    create controllers and models, refactoring server.js

---
+++ controllers/users.js
@@ -0,0 +1,56 @@
+let Users = require('../models/users');
+
+exports.all = (req, res) => {
+    Users.all((err, docs) => {
+        if (err) {
+            console.log(err);
+            return res.senfStatus(500);
+        }
+        res.send(docs);
+    })
+}
+
+exports.findById = (req, res) => {
+    Users.findById(req.params.id, (err, doc) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(doc);
+    })
+}
+
+exports.create = (req, res) => {
+    let user = {
+        name: req.body.name,
+    };
+    Users.create(user, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.send(user);
+        
+    })
+}
+
+exports.update = (req, res) => {
+    Users.update(req.params.id, {$set: {name: req.body.name}}, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.sendStatus(200);
+        
+    })
+}
+
+exports.delete = (req, res) => {
+    Users.delete(req.params.id, (err, result) => {
+        if (err) {
+            console.log(err);
+            return res.sendStatus(500);
+        }
+        res.sendStatus(200);
+    })
+}
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 let express = require('express');
 let bodyParser = require('body-parser');
-let ObjectID = require('mongodb').ObjectID;
-
 let dataBase = require('./dataBase');
+let usersController = require('./controllers/users')
 
 let app = express();
 
@@ -19,68 +18,12 @@ dataBase.connect('mongodb://localhost:27017', {useUnifiedTopology: true, useNewU
     })
 })
 
-app.get('/', (req, res) => {
-    res.send('Hello API');
-})
+app.get('/users', usersController.all)
 
-app.get('/users', (req, res) => {
-    dataBase.get().collection('users').find().toArray((err, docs) => {
-        if (err) {
-            console.log(err);
-            return res.sendStatus(500);
-        }
-        res.send(docs);
-    })
-})
+app.get('/users/:id', usersController.findById)
 
-app.get('/users/:id', function (req, res) { // :id это динамический айдишник. То есть http://localhost:3012/users/1 или http://localhost:3012/users/2 разницы нет. В зависимости от кода дальше будет отдавать определенные данные
-    // все айдишники в моного это специальные ObjectID. Чтобы найти какой-то документ нам необходимо строку айдишника конвертировать в ObjectID
-    dataBase.get().collection('users').findOne({_id: ObjectID(req.params.id)}, (err, doc) => {
-        if (err) {
-            console.log(err);
-            return res.sendStatus(500);
-        }
-        res.send(doc);
-    })
-})
+app.post('/users', usersController.create)
 
-app.post('/users', function (req, res) { // метод post реализует добавление пользователя в базу данных
-    let user = {
-        name: req.body.name,
-    };
-    dataBase.get().collection('users').insertOne(user, (err, result) => {
-        // обращение к коллекции users. Если коллекции не существует, то она создасться автоматически. На вход в insert нужно передать объект, который мы хоти сохранить, вторым параметром идет функция с параметрами err и result
-        if (err) {
-            console.log(err);
-            return res.sendStatus(500);
-        }
-        res.send(user);
-    })
-})
+app.put('/users/:id', usersController.update)
 
-app.put('/users/:id', (req, res) => {
-    dataBase.get().collection('users').updateOne(
-        {_id: ObjectID(req.params.id)},
-        {$set: {name: req.body.name}},
-        (err, result) => {
-            if (err) {
-                console.log(err);
-                return res.sendStatus(500);
-            }
-            res.sendStatus(200);
-        }
-    )
-})
-
-app.delete('/users/:id', function (req, res) {
-    dataBase.get().collection('users').deleteOne(
-        {_id: ObjectID(req.params.id)},
-        (err, result) => {
-            if (err) {
-                console.log(err);
-                return res.sendStatus(500);
-            }
-            res.sendStatus(200);
-        }
-    )
-})
+app.delete('/users/:id', usersController.delete)
