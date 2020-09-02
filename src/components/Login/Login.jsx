21f02afa00d5b2701a66ef51a9ef79f3365d1cb1
dmitriy <lendmid@bk.ru>
2020-09-02 22:37:27 +0300

    added redux-form in loginPage

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,41 @@
 import React from "react"
+import s from './Login.module.css';
+import {Field, reduxForm} from "redux-form"
 
 
-const Login = (props) => {
+let Login = (props) => {
+    let onSubmit = (formData) => {
+        console.log(formData);
+    }
+    
     return (
-        <div>
+        <div className={s.wrapper}>
             <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit} />
         </div>
     )
 }
+
+let LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit} className={s.form} >
+            <div>
+                <label htmlFor="login">Login</label>
+                <Field component="input" type="text" placeholder="Login" name="login" />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <Field component="input" type="password" placeholder="Password" name="password" />
+            </div>
+            <div>
+                <label htmlFor="checkbox">Remember me</label>
+                <Field component="input" type="checkbox" name="checkbox" />
+            </div>
+            <button>Login</button>
+        </form>
+    )
+}
+
+let LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
 export default Login;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,13 +3,15 @@ import profileReducer from "./profileReducer";
 import messegesReducer from "./messagesReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
-import thunkMiddleware from "redux-thunk"
+import thunkMiddleware from "redux-thunk";
+import {reducer as formReducer} from "redux-form"
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     messagesPage: messegesReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
