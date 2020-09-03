2e131116746273989953e862b98b5ccc44910a5b
dmitriy <lendmid@bk.ru>
2020-09-04 00:51:01 +0300

    fixed authorisation, added button signOut in Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from './Header.module.css';
 import {NavLink} from "react-router-dom";
+import signOut from "../../assets/images/signOut.svg"
 
 const Header = (props) => {
     return (
@@ -9,11 +10,15 @@ const Header = (props) => {
                 <ul className={s.list}>
                     <li><NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink></li>
                     <li><NavLink to="/messages">Messages</NavLink></li>
-                    <li><NavLink to="">News</NavLink></li>
+                    <li><NavLink to="/users">Users</NavLink></li>
                     <li><NavLink to="">Music</NavLink></li>
                     <li><NavLink to="">Settings</NavLink></li>
                 </ul>
             </nav>
+            <button className={s.signOut}>
+                <img src={signOut} alt="signOut" className={s.img}/>
+                <span className={s.span}>Sign out</span>
+            </button>
         </header>
     )
 }
