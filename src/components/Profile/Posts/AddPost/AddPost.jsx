f92e1c29473ac7ef207923f74a0b62486f978c0b
dmitriy <lendmid@bk.ru>
2020-09-07 15:47:30 +0300

    worked with addPost component. trouble with react-form

--- src/components/Profile/Posts/AddPost/AddPost.jsx
+++ src/components/Profile/Posts/AddPost/AddPost.jsx
@@ -8,40 +8,75 @@ import {TextAreaProfilePage} from "../../../common/FormValidator/FormValidator";
 let minLenght1 = minLenghtCreator(1)
 
 let AddPost = (props) => {
-    
-    const { handleSubmit, reset, submitting } = props
-    
     let addNewPost = (formData) => {
-        debugger
         props.addPost(formData.newPostText);
     }
     
     return (
         <div className={s.add_post_wrapper}>
-            <form onSubmit={handleSubmit}>
-                <Field component={TextAreaProfilePage} className={s.textarea} name="newPostText" cols="30" rows="7" placeholder="Enter somesing..." validate={[required, minLenght1]}></Field>
-                <button className={s.button} type="submit" disabled={submitting} >Add post</button>
-                {/*<button className={s.button} type="submit" disabled={submitting} onClick={reset}>Add post</button>*/}
-            </form>
-            {/*<AddPostFormRedux onSubmit={addNewPost} />*/}
+            <AddPostFormRedux onSubmit={addNewPost} />
         </div>
     )
 }
 
-// let AddPostForm = (props) => {
+let AddPostForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <Field component={TextAreaProfilePage} className={s.textarea} name="newPostText" cols="30" rows="7" placeholder="Enter somesing..." validate={[required, minLenght1]}></Field>
+            <button className={s.button}>Add post</button>
+        </form>
+    )
+}
+
+let AddPostFormRedux = reduxForm({form: 'addPost'})(AddPostForm)
+
+export default AddPost;
+
+
+// import React from "react";
+// import s from './AddPost.module.css';
+// import {Field, reduxForm} from "redux-form"
+// import {minLenghtCreator, required} from "../../../../utils/validators/validators";
+// import {TextAreaProfilePage} from "../../../common/FormValidator/FormValidator";
+//
+//
+// let minLenght1 = minLenghtCreator(1)
+//
+// let AddPost = (props) => {
+//
+//     const { handleSubmit, reset, submitting } = props
+//
+//     let addNewPost = (formData) => {
+//         debugger
+//         props.addPost(formData.newPostText);
+//     }
+//
 //     return (
-//         <form onSubmit={props.handleSubmit}>
-//             <Field component={TextAreaProfilePage} className={s.textarea} name="newPostText" cols="30" rows="7" placeholder="Enter somesing..." validate={[required, minLenght1]}></Field>
-//             <button className={s.button} onClick={reset}>Add post</button>
-//         </form>
+//         <div className={s.add_post_wrapper}>
+//             <form onSubmit={handleSubmit}>
+//                 <Field component={TextAreaProfilePage} className={s.textarea} name="newPostText" cols="30" rows="7" placeholder="Enter somesing..." validate={[required, minLenght1]}></Field>
+//                 <button className={s.button} type="submit" disabled={submitting} >Add post</button>
+//                 {/*<button className={s.button} type="submit" disabled={submitting} onClick={reset}>Add post</button>*/}
+//             </form>
+//             {/*<AddPostFormRedux onSubmit={addNewPost} />*/}
+//         </div>
 //     )
 // }
-
-// let AddPostFormRedux = reduxForm({form: 'addPost'})(AddPostForm)
-// let AddPostFormRedux = reduxForm({form: 'addPost'})(AddPostForm)
-
-export default reduxForm({
-    form: 'addPost',
-    // onSubmit: values => console.log('sended', values),
-    // onSubmit: formData => props.addPost(formData.newPostText)
-})(AddPost);
+//
+// // let AddPostForm = (props) => {
+// //     return (
+// //         <form onSubmit={props.handleSubmit}>
+// //             <Field component={TextAreaProfilePage} className={s.textarea} name="newPostText" cols="30" rows="7" placeholder="Enter somesing..." validate={[required, minLenght1]}></Field>
+// //             <button className={s.button} onClick={reset}>Add post</button>
+// //         </form>
+// //     )
+// // }
+//
+// // let AddPostFormRedux = reduxForm({form: 'addPost'})(AddPostForm)
+// // let AddPostFormRedux = reduxForm({form: 'addPost'})(AddPostForm)
+//
+// export default reduxForm({
+//     form: 'addPost',
+//     // onSubmit: values => console.log('sended', values),
+//     // onSubmit: formData => props.addPost(formData.newPostText)
+// })(AddPost);
