2071ce8cf92eacd653c71d6c85cffeb2038fca3f
dmitriy <lendmid@bk.ru>
2020-09-09 22:03:21 +0300

    added reselect selector in usersSelectors

--- src/redux/usersSelectors.js
+++ src/redux/usersSelectors.js
@@ -1,6 +1,13 @@
+import {createSelector} from "reselect"
+
+
 export let getUsers = state => {
     return state.usersPage.users;
 }
+
+export let getUsersSelector = createSelector(getUsers, users => {
+    return users.filter(u => true);
+})
 
 export let getPageSize = state => {
     return state.usersPage.pageSize;
