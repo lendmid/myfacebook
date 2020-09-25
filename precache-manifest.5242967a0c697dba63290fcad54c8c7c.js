50f51bf78432a807e1f1b8167187894b6e8e13a5
dmitriy <lendmid@bk.ru>
2020-09-25 18:50:10 +0300

    Updates

---
+++ precache-manifest.5242967a0c697dba63290fcad54c8c7c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeba93f107b8139f00881f11bac12447",
+    "url": "/myfacebook/index.html"
+  },
+  {
+    "revision": "5a6f48ca214748e4104b",
+    "url": "/myfacebook/static/css/main.5c074c2c.chunk.css"
+  },
+  {
+    "revision": "d08004e775478699a3b4",
+    "url": "/myfacebook/static/js/2.ee5d6be5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myfacebook/static/js/2.ee5d6be5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a6f48ca214748e4104b",
+    "url": "/myfacebook/static/js/main.dba1ad98.chunk.js"
+  },
+  {
+    "revision": "74af69762df771e5fa85",
+    "url": "/myfacebook/static/js/runtime-main.8c626f2f.js"
+  },
+  {
+    "revision": "58c73352f8542dd918d0ca0552152a1d",
+    "url": "/myfacebook/static/media/Preloader.58c73352.svg"
+  },
+  {
+    "revision": "e01b7d2bc984079de9858a12327aafee",
+    "url": "/myfacebook/static/media/add_icon.e01b7d2b.svg"
+  },
+  {
+    "revision": "6d68e718a0334b71f04008e8e4816f34",
+    "url": "/myfacebook/static/media/avatar_3.6d68e718.png"
+  },
+  {
+    "revision": "0811f40d2159867ceac8b8f65a8ffe94",
+    "url": "/myfacebook/static/media/avatar_background.0811f40d.jpg"
+  },
+  {
+    "revision": "04a047319d8a7e591d38c9d124f22a1d",
+    "url": "/myfacebook/static/media/logOut.04a04731.svg"
+  },
+  {
+    "revision": "8c0f81a2c73452931757cfd75dd0d3be",
+    "url": "/myfacebook/static/media/preview_profile.8c0f81a2.svg"
+  },
+  {
+    "revision": "f07d66e00150e23a0d8b2e10b7b0f86b",
+    "url": "/myfacebook/static/media/user_avatar.f07d66e0.png"
+  }
+]);
