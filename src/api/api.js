f3c82f22a1da48d83b5e023f86429868ccab9018
dmitriy <lendmid@bk.ru>
2020-09-25 18:59:28 +0300

    changed API KEY

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
-        "API-KEY": "b1775b2f-c3a5-4509-8dc9-90b5629de7c3"
+        "API-KEY": "e5b13819-4976-401b-b8d2-ba90bbee1253"
     },
 })
 
