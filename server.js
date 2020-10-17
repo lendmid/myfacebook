f82b8f6a3d687834987952648f2364273c1d4dcc
dmitriy <lendmid@bk.ru>
2020-10-17 22:57:14 +0300

    started write self API

--- server.js
+++ server.js
@@ -1,28 +1,52 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let dataBase = require('./dataBase');
-let usersController = require('./controllers/users')
-let app = express();
+const express = require('express');
+const app = express();
+// requare first API
+const bodyParser = require('body-parser');
+const dataBase = require('./dataBase');
+const usersController = require('./controllers/users');
+// requare new API
+const mongoose = require('mongoose');
+const config = require('config');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+const PORT = config.get('port') || 5000
 
-dataBase.connect('mongodb://localhost:27017', {useUnifiedTopology: true, useNewUrlParser: true},err => {
-    if (err) return console.log(err);
-    
-    console.log('DataBase connected');
-    
-    app.listen(3012, () => {
-        console.log('API app started')
-    })
-})
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        });
+        app.listen(PORT, () => console.log(`API app has been started on port ${PORT}...`));
+    } catch (e) {
+        console.log('Server Error', e.message);
+        process.exit(1);
+    }
+}
 
-app.get('/users', usersController.all)
+start();
 
-app.get('/users/:id', usersController.findById)
 
-app.post('/users', usersController.create)
-
-app.put('/users/:id', usersController.update)
-
-app.delete('/users/:id', usersController.delete)
+//mongoDB, login: lendmid, pass: JZaDz1tCMC8l7z8X
+/////
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
+//
+// dataBase.connect('mongodb://localhost:27017', {useUnifiedTopology: true, useNewUrlParser: true},(err) => {
+//     if (err) return console.log(err);
+//
+//     console.log('DataBase connected');
+//
+//     app.listen(PORT, () => {
+//         console.log(`API app has been started on port ${PORT}...`)
+//     })
+// })
+//
+// app.get('/users', usersController.all)
+// app.get('/users/:id', usersController.findById)
+//
+// app.post('/users', usersController.create)
+//
+// app.put('/users/:id', usersController.update)
+//
+// app.delete('/users/:id', usersController.delete)
