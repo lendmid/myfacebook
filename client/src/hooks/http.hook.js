dd9004f12e502a134ad4205ad6af39b37cccfe3f
dmitriy <lendmid@bk.ru>
2020-10-22 23:52:31 +0300

    worked on API

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -1,3 +1,16 @@
-export const useHttp = () => {
+import {useCallback, useState} from 'react';
 
-}
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {}) => {
+        try {
+            await fetch(url, {method, body, headers})
+        } catch (e) {
+        
+        }
+    });
+    
+    return {loading, request, error}
+};
